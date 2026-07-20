80e3ca8eb6be81c3a2ed1c254f619aaacfd6b2e5
linocai <linocai13@gmail.com>
2026-07-20 10:43:24 +0800

    补:THS 概念接口无 token 优雅降级单测(ts_ths_index/member/daily)

--- tests/test_tushare_client.py
+++ tests/test_tushare_client.py
@@ -86,6 +86,9 @@ class TestNoTokenGracefulDegradation:
             (ts_mod.ts_trade_cal, dict(start="20260101", end="20260110")),
             (ts_mod.ts_stock_basic, dict(list_status="L")),
             (ts_mod.ts_namechange_page, dict()),
+            (ts_mod.ts_ths_index, dict()),
+            (ts_mod.ts_ths_member, dict(index_code="883300.TI")),
+            (ts_mod.ts_ths_daily, dict(index_code="883300.TI", start="20240101", end="20240110")),
         ]:
             res = fn(**kwargs)
             assert res.ok is False, f"{fn.__name__} 应在无 token 时优雅降级"
